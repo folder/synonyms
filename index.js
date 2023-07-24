50a53d0bfaeae60800f0593d74ab3ce7b29108de
Jon Schlinkert <jon.schlinkert@sellside.com>
2023-07-24 11:40:55 -0400

    2.0.0 - adds OpenAI API support. AI powered synonyms!
    Allows options to be passed for any LLM from OpenAI (gpt-3, gpt-4, etc)

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+'use strict';
+
 require('dotenv/config');
 
 const url = 'https://wordsapiv1.p.rapidapi.com/words';
 const SPLIT_REGEX = /[\s-]+/g;
 const JOIN_CHARS = false;
+
+const defaults = {
+  model: 'gpt-3.5-turbo-0613',
+  temperature: 1,
+  max_tokens: 150,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  n: 1
+};
 
 const option = (value, fallback) => {
   if (value === false) return false;
@@ -43,6 +55,50 @@ const splitWords = (words = [], options = {}) => {
   return [...new Set([...array, ...parsed])].sort();
 };
 
+const promptSynonyms = async ({
+  apiKey = process.env.OPENAI_API_KEY,
+  words = [],
+  config
+}) => {
+  const res = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      ...defaults,
+      ...config,
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful AI assistant. Return a list of comma-separated synonyms or words that are similar to what the user requests. You strongly prefer single word synonyms, compound words, or words with a dash; and you avoid phrases with multiple words separated by spaces. You strongly dislike adding any commentary, explanations, descriptions, or other words that are not synonyms.'
+        },
+        {
+          role: 'user',
+          content: [].concat(words).join(', ')
+        }
+      ]
+    })
+  });
+
+  if (!res.ok) {
+    throw new Error(await res.text() || 'Failed to fetch the chat response.');
+  }
+
+  if (!res.body) {
+    throw new Error('The response body is empty.');
+  }
+
+  const { choices, ...data } = await res.json();
+  const result = { ...data };
+
+  result.input = [].concat(words).join(', ');
+  result.message = choices[0].message;
+  result.synonyms = result.message.content.split(',').map(s => s.trim());
+  return result;
+};
+
 const fetchSynoyms = async (url, options) => {
   try {
     const response = await fetch(url, options);
@@ -58,16 +114,23 @@ const fetchSynoyms = async (url, options) => {
     }
 
     return response;
-  } catch (err) {
-
+  } catch (error) {
+    options.onError?.({ error });
+    return Promise.reject(error);
   }
 };
 
 const synonyms = async (words = [], options = {}) => {
   const apiKey = options.apiKey || process.env.WORDS_API_KEY;
+  const openAIKey = options.openAIKey || process.env.OPENAI_API_KEY;
   const values = options?.split ? splitWords(words, options) : [].concat(words);
   const pending = new Set();
   const results = { all: [], words: {} };
+
+  if (options.openai) {
+    const config = options.openai === true ? {} : options.openai;
+    return promptSynonyms({ apiKey: openAIKey, words: values, config });
+  }
 
   const defaults = {
     method: 'GET',
@@ -109,3 +172,5 @@ const synonyms = async (words = [], options = {}) => {
 module.exports = synonyms;
 module.exports.synonyms = synonyms;
 module.exports.splitWords = splitWords;
+
+synonyms('5 words that mean "mildly fabulous"', { openai: true }).then(console.log);
